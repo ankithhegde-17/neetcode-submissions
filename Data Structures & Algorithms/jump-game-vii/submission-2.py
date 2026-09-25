0b737dd8396415be584d6c71908f73fe3f643c96
Ankith Hegde <ankithhegde001@gmail.com>
2026-09-25 07:01:26 +0530

    Add: jump-game-vii - submission-2

---
+++ Data Structures & Algorithms/jump-game-vii/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, s: str, minJump: int, maxJump: int) -> bool:
+        n = len(s)
+        dp = [False] * n
+        dp[0] = True
+        reachable = 0
+
+        for i in range(1, n):
+            if i - minJump >= 0:
+                reachable += dp[i - minJump]
+
+            if i - maxJump - 1 >= 0:
+                reachable -= dp[i - maxJump - 1]
+
+            if s[i] == '0' and reachable > 0:
+                dp[i] = True
+
+        return dp[-1]
